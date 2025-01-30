6544f7eff1a31bc89a55f1cc75811b6efdc9745a
Jessa Tenebro <163012535+celestial203@users.noreply.github.com>
2025-01-30 13:48:04 +0800

    login fb

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// index.js or app.js (or another entry point in your app)
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCvI3y5mQ4TWOhbnHH6OSB3GzeNcFpe7LM",
   authDomain: "cenro-3ffc4.firebaseapp.com",
@@ -15,11 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Log Firebase initialization after the app is initialized
-console.log('Firebase initialized:', app);
-
-// Initialize Analytics (optional for tracking events)
 const analytics = getAnalytics(app);
-
-// Your app logic here
