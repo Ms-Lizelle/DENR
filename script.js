fd904110d69fced387cd29aa8cdc92f59a33011c
Jessa Tenebro <163012535+celestial203@users.noreply.github.com>
2025-01-30 13:24:02 +0800

    log in auth

--- script.js
+++ script.js
@@ -8,15 +8,14 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyCOXtfjmfFhy-Ya6fUCyj8d23D0F1pgL7o",
-    authDomain: "cenroit.firebaseapp.com",
-    projectId: "cenroit",
-    storageBucket: "cenroit.firebasestorage.app",
-    messagingSenderId: "880361469888",
-    appId: "1:880361469888:web:603fe8dfa389d3b6d6ba25",
-    measurementId: "G-52XKVXFHLS"
+    apiKey: "AIzaSyCvI3y5mQ4TWOhbnHH6OSB3GzeNcFpe7LM",
+    authDomain: "cenro-3ffc4.firebaseapp.com",
+    projectId: "cenro-3ffc4",
+    storageBucket: "cenro-3ffc4.firebasestorage.app",
+    messagingSenderId: "763027479113",
+    appId: "1:763027479113:web:e7ff66db1267c091c77d98",
+    measurementId: "G-CQSD8JRXMT"
   };
-
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
